132465d5aa9d2f534ef770a27e5c814e4fa61218
aadilraza339 <aadil18@navgurukul.org>
2024-10-16 21:03:35 +0530

    update code

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, './frontend/build')));
-// const seats = require('./frontend/build')
-// The "catchall" handler: for any request that doesn't
-// match one above, send back the React app.
+app.use(express.static(path.join(__dirname, './frontent-client/build')));
+
 app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, './frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, './frontent-client/build', 'index.html'));
 });
 
 app.get('/api/avaiable/:seat', (req, res) => {
